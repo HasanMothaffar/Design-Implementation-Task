846618c9be063b0091d2fd0ff0bb01a67722e70f
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-09-26 10:00:58 +0300

    Add action buttons to the hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,70 @@
-import { Box, Center, Heading, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import BaseContainer from "./shared/BaseContainer";
 
-const AuthButtons = () => {};
+const ActionButtons = () => {
+    return (
+        <Flex color="#fff" margin="0 -20px" overflow="hidden">
+            <Box flexGrow={1} bgColor="#21424a" transform="skew(-20deg)" p={8}>
+                <Link
+                    textAlign="center"
+                    bgColor="#21424a"
+                    transform="skew(20deg)"
+                    display="block"
+                    fontSize={["md", "xl"]}
+                    textTransform="uppercase"
+                >
+                    Login
+                </Link>
+            </Box>
+
+            <Box flexGrow={1} bgColor="#2fac6c" transform="skew(-20deg)" p={8}>
+                <Link
+                    textAlign="center"
+                    bgColor="#2fac6c"
+                    transform="skew(20deg)"
+                    display="block"
+                    fontSize={["md", "xl"]}
+                    textTransform="uppercase"
+                >
+                    Register
+                </Link>
+            </Box>
+        </Flex>
+    );
+};
 
 const Hero = () => {
     return (
-        <Box
-            backgroundImage="url(/homebg.jpg)"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            as="section"
-        >
-            <Center
-                as="header"
-                backgroundColor="rgba(0, 0, 0, 0.60)"
-                flexDirection="column"
-                color="#FFF"
-                minHeight="800px"
+        <>
+            <Box
+                backgroundImage="url(/homebg.jpg)"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                as="section"
             >
-                <Heading as="h1" fontSize={["xl", "7xl"]} textTransform="uppercase" mb={4}>
-                    Hello!
-                </Heading>
-                <Text as="p" fontSize="4xl" fontWeight="bold" fontStyle="italic">
-                    We love our work...
-                </Text>
-                <Text as="p" fontSize="4xl" fontWeight="bold" fontStyle="italic">
-                    And We're taking it seriously...
-                </Text>
-            </Center>
-        </Box>
+                <Center
+                    as="header"
+                    backgroundColor="rgba(0, 0, 0, 0.60)"
+                    flexDirection="column"
+                    color="#FFF"
+                    minHeight="800px"
+                    textAlign="center"
+                >
+                    <BaseContainer>
+                        <Heading as="h1" fontSize={["5xl", "7xl"]} textTransform="uppercase" mb={4}>
+                            Hello!
+                        </Heading>
+                        <Text as="p" fontSize={["2xl", "4xl"]} fontWeight="bold" fontStyle="italic">
+                            We love our work...
+                        </Text>
+                        <Text as="p" fontSize={["2xl", "4xl"]} fontWeight="bold" fontStyle="italic">
+                            And We're taking it seriously...
+                        </Text>
+                    </BaseContainer>
+                </Center>
+            </Box>
+            <ActionButtons />
+        </>
     );
 };
 export default Hero;
